f5f7681faca67ad89f6c06e7666c64d46c6ca11e
Emupedia <admin@emupedia.net>
2019-12-21 17:55:01 +0200

    More work

--- beta/emuos/vfat/games/half-life/js/main.js
+++ beta/emuos/vfat/games/half-life/js/main.js
@@ -222,6 +222,7 @@
 								throw new Error(xhr.statusText + " : " + xhr.responseURL);
 							}
 						};
+						xhr.setRequestHeader('X-File-Name', packageName);
 						xhr.send(null);
 					}).catch(function (error) {
 						console.log(error);
@@ -294,7 +295,7 @@
 
 			// noinspection JSUnresolvedVariable
 			if (SYSTEM_FEATURE_CANVAS && SYSTEM_FEATURE_TYPED_ARRAYS && SYSTEM_FEATURE_ASMJS) {
-				/*if (SYSTEM_FEATURE_SERVICE_WORKERS) {
+				if (SYSTEM_FEATURE_SERVICE_WORKERS) {
 					navigator.serviceWorker.register('sw.js').then(function(registration) {
 						registration.addEventListener('updatefound', function() {
 							var installingWorker = registration.installing;
@@ -305,7 +306,7 @@
 					});
 				} else {
 					console.log('ServiceWorker not supported');
-				}*/
+				}
 
 				window.Module = {
 					TOTAL_MEMORY: 150 * 1024 * 1024,
--- beta/emuos/vfat/games/half-life/sw.js
+++ beta/emuos/vfat/games/half-life/sw.js
@@ -1,10 +1,61 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+function fetchFromCache(e) {
+	console.log(e.request.headers.get('X-File-Name'));
 
-// noinspection JSUnresolvedVariable
-if (workbox) {
-	// noinspection JSUnresolvedVariable,JSUnresolvedFunction
-	workbox.routing.registerRoute(/^https:\/\/dl\.dropboxusercontent\.com/, new workbox.strategies.CacheFirst({
-		cacheName: 'data',
-		cacheableResponse: {statuses: [0, 200]}
-	}));
+	return caches.match(e.request.headers.get('X-File-Name')).then(function(response) {
+		if (!response) {
+			throw Error(e.request.url + ' not found in cache');
+		}
+
+		return response;
+	});
 }
+
+function addToCache(name, request, response) {
+	if (response.ok) {
+		var content_type = response.headers.get('Content-Type') || '';
+		var filename = request.headers.get('X-File-Name') || '';
+
+		if (content_type === 'application/zip') {
+			if (filename !== '') {
+				console.log(content_type);
+				console.log(filename);
+				var copy = response.clone();
+
+				caches.open(name).then(function(cache) {
+					// noinspection JSIgnoredPromiseFromCall
+					cache.put(filename, copy);
+				});
+			}
+		}
+	}
+
+	return response;
+}
+
+self.addEventListener('install', function() {
+	console.log('ServiceWorker Installed!');
+});
+
+self.addEventListener('activate', function() {
+	console.log('ServiceWorker Activated!');
+});
+
+self.addEventListener('message', function(e) {
+	console.log('ServiceWorker: Handling message event: ', e);
+});
+
+self.addEventListener('fetch', function(e) {
+	console.log('ServiceWorkder: Handling fetch event for: ', e.request.url);
+
+	if (e.request.url.startsWith('https://dl.dropboxusercontent.com')) {
+		var request = e.request;
+
+		// noinspection BadExpressionStatementJS
+		e.respondWith(fetchFromCache(e).catch(function() {
+			return fetch(request);
+		}).then(function(response){
+			return addToCache('data', request, response);
+		}));
+	}
+});
+
