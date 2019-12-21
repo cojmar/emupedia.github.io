5f6f497502c73a8977fba1b89c647ecdff26bb9f
Emupedia <admin@emupedia.net>
2019-12-21 15:20:26 +0200

    More work

--- beta/emuos/vfat/games/half-life/js/sw.js
+++ beta/emuos/vfat/games/half-life/js/sw.js
@@ -3,5 +3,5 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 // noinspection JSUnresolvedVariable
 if (workbox) {
 	// noinspection JSUnresolvedVariable,JSUnresolvedFunction
-	workbox.routing.registerRoute(/^https:\/\/dl.dropboxusercontent.com/, new workbox.strategies.StaleWhileRevalidate());
+	workbox.routing.registerRoute(/^https:\/\/dl\.dropboxusercontent\.com/, new workbox.strategies.StaleWhileRevalidate());
 }
