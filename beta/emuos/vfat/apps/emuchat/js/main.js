8740d3875eade45f2d2efe20bb0e3d51acad5b9e
Emupedia <admin@emupedia.net>
2020-01-10 13:23:05 +0200

    More work

--- beta/emuos/vfat/apps/emuchat/js/main.js
+++ beta/emuos/vfat/apps/emuchat/js/main.js
@@ -110,15 +110,7 @@
 			net.colors = ['rgba(180, 173, 173, 0.973)', '#395fa4', '#159904', 'rgba(128, 128, 128, 0.35)'];
 
 			net.normalize = function(str) {
-				var result = str;
-
-				if (typeof String.prototype.normalize === 'function') {
-					try {
-						result = str.normalize('NFKD').replace(/[^\w\s.-/`~!@#$%^&*()-_=+\[\]{}'"|\/]/gi, '');
-					} catch (e) {}
-				}
-
-				return result;
+				return $('<div />').text(str.replace(/[^\w\s.-/`~!@#$%^&*()-_=+\[\]{}'"|\/]/gi, '')).html();
 			};
 
 			// noinspection DuplicatedCode
@@ -228,8 +220,9 @@
 				net.text_input.val('');
 			};
 
+			// noinspection DuplicatedCode
 			net.socket.on('connect', function(data) {
-				var nickname = typeof simplestorage.get('nickname') !== 'undefined' ? $('<div />').text(simplestorage.get('nickname')).html() : 'EMU-' + fingerprint;
+				var nickname = typeof simplestorage.get('nickname') !== 'undefined' ? net.normalize(simplestorage.get('nickname')) : 'EMU-' + fingerprint;
 				var server = typeof data !== 'undefined' ? data.server : net.server;
 				// noinspection JSUnresolvedVariable
 				var socket_id = typeof data !== 'undefined' ? data.socket_id : net.socket.id;
@@ -244,8 +237,7 @@
 			});
 
 			net.socket.on('auth.info', function (data) {
-				var name = $('<div />').text(data.info.user).html();
-				simplestorage.set('nickname', name);
+				simplestorage.set('nickname', net.normalize(data.info.user));
 			});
 
 			// noinspection DuplicatedCode
@@ -254,6 +246,7 @@
 
 				for (var n in data.users) {
 					var color = (n !== data.me) ? net.colors[3] : net.colors[1];
+					// noinspection JSUnfilteredForInLoop
 					var name = net.normalize(n);
 					// noinspection JSUnfilteredForInLoop
 					r_users += '<div id="room_user_' + name + '" style="color: ' + color + '; overflow: hidden;">' + name + '</div>';
